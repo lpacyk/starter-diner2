b169321258c86bafcb5f0f131d4a55a90670331b
Lukasz Pacyk <pacyklukasz@gmail.com>
2016-11-20 16:57:51 -0800

    Created Model for order Created functioning Shopping controller and methods for adding, canceling an order and displaying the receipt.
    Modified following templates to show order information
    template-shopping.php
    summary.php
    menuitem-shop.php
    Signed-off-by:Lukasz Pacyk <pacyklukasz@gmail.com>

--- application/controllers/Shopping.php
+++ application/controllers/Shopping.php
@@ -5,28 +5,80 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Shopping extends Application
 {
 
-	function __construct() {
-		parent::__construct();
-	}
-	
-	public function index()
-	{
-        $stuff = file_get_contents('../data/receipt.md');
-        $this->data['receipt'] = $this->parsedown->parse($stuff);
-		$this->data['content'] = '';
-		
-		// pictorial menu
-		$count = 1;
-		foreach ($this->categories->all() as $category) {
-			$chunk = 'category' . $count; 
-			$this->data[$chunk] = $this->parser->parse('category-shop',$category,true);
-			foreach($this->menu->all() as $menuitem) {
-				if ($menuitem->category != $category->id) continue;
-				$this->data[$chunk] .= $this->parser->parse('menuitem-shop',$menuitem,true);
-			}
-			$count++;
-		}
-		$this->render('template-shopping'); 
-	}
+        function __construct($state=null) {
+            parent::__construct();
+            if (is_array($state)) {
+                foreach($state as $key => $value)
+                    $this->$key = $value;
+            } else {
+                $this->number = 0;
+                $this->datetime = null;
+                $this->items = array();
+            }
+        }
+        
+        
+        public function index() {
+            // What is the user up to?
+            if ($this->session->has_userdata('order'))
+                $this->keep_shopping();
+            else $this->summarize();
+        }
 
+        public function summarize() {
+            $this->data['pagebody'] = 'summary';
+            $this->render('template');  // use the default template
+        }
+
+          public function keep_shopping() {
+            $this->data['content'] = '';
+            // pictorial menu
+            $count = 1;
+            foreach ($this->categories->all() as $category) {
+                    $chunk = 'category' . $count; 
+                    $this->data[$chunk] = $this->parser->parse('category-shop',$category,true);
+                    foreach($this->menu->all() as $menuitem) {
+                            if ($menuitem->category != $category->id) continue;
+                            $this->data[$chunk] .= $this->parser->parse('menuitem-shop',$menuitem,true);
+                    }
+                    $count++;
+            }
+
+            $order = new Order($this->session->userdata('order'));
+            $stuff = $order->receipt();
+            $this->data['receipt'] = $this->parsedown->parse($stuff);
+
+            $this->render('template-shopping');
+    }
+    
+        
+        public function neworder() {
+            // create a new order if needed
+            if (! $this->session->has_userdata('order')) {
+                $order = new Order();
+                $this->session->set_userdata('order', (array) $order);
+            }
+
+            $this->keep_shopping();
+        }
+        
+        public function cancel() {
+            // Drop any order in progress
+            if ($this->session->has_userdata('order')) {
+                $this->session->unset_userdata('order');
+            }
+
+            $this->index();
+        }
+        
+        public function add($what) {
+            $order = new Order($this->session->userdata('order'));
+            $order->additem($what);
+            $this->keep_shopping();
+            $this->session->set_userdata('order',(array)$order);
+            redirect('/shopping');
+        }
+        
+        
+        
 }
